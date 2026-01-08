eadc749af4159d25414fa1f44ebf18bd81dbe147
Baykov Aleksandr <baykov-1988@mail.ru>
2026-01-08 15:52:14 +0300

    add search component

--- films/src/App.jsx
+++ films/src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import svg from "./assets/search-normal.svg";
 import Header from "./components/Layout/Header/Header";
+import Search from './components/Search/Search';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Search iconSrc={svg} />
     </div>
   );
 }
